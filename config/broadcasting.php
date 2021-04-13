79b41a90bff9c683d05a698307d5bb95fcdccd0e
Simon Auer <simon@du.digital>
2021-04-13 13:42:55 +0200

    Add TestBroadcastCreated Event to test websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -49,7 +49,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                'encrypted' => false,
                 'host' => '127.0.0.1',
                 'port' => env('LARAVEL_WEBSOCKETS_PORT', 6001),
                 'scheme' => 'http'
